eb390f2dda72ba311d8093762cf518ee4d749e81
downpour07 <choiheeyoung10@gmail.com>
2023-11-08 10:23:34 +0900

    ADD :: Login

---
+++ src/pages/login.jsx
@@ -0,0 +1,24 @@
+import react from 'react';
+import styled from 'styled-components';
+import axios from "axios"
+
+function loginPage() {
+  return (
+    <>
+      <Background>
+        <div></div>
+      </Background>
+    </> 
+  )
+};
+
+const Background = styled.div`
+  width: 100%;
+`
+const Email = styled.div`
+  width: 100%;
+`
+const Signup = styled.div`
+  width: 100%;
+`
+export default loginPage;
