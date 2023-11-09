f4e9501600bc994299cb36f18170b4ca83318ac6
downpour07 <choiheeyoung10@gmail.com>
2023-11-09 17:15:59 +0900

    CHORE :: 파일 경로 수정

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import {
   Routes, Outlet
 } from "react-router-dom";
 import Header from "./components/header";
-import Mainpage from "./pages/login";
+// import Mainpage from "./pages/main";
 import Loginpage from "./pages/login";
-<<<<<<< Updated upstream
-import Seoul from "./pages/Seoul";
-=======
-import Signuppage from "./pages/login";
->>>>>>> Stashed changes
+// import Seoul from "./pages/Seoul";
+import Signuppage from "./pages/signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Header />} >
-          <Route path="/" element={<Mainpage />} />
+          {/* <Route path="/" element={<Mainpage />} /> */}
           <Route path="/login" element={<Loginpage />} />
-<<<<<<< Updated upstream
-          <Route path="/Seoul" element={<Seoul />} />
-=======
+          {/* <Route path="/Seoul" element={<Seoul />} /> */}
           <Route path="/signup" element={<Signuppage />} />
->>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
